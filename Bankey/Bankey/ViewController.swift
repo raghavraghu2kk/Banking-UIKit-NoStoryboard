36ac0568a46ca8593ba82b941251686e51288471
Raghavendra S Mirajkar <raghavraghu2001@gmail.com>
2024-08-01 22:34:47 +0530

    commiitng

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Raghavendra Mirajkar on 01/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
+
